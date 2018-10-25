5e933546d49de544266903fa23fea72a6a54e387
ErickaDelmonte <erickaabdon56@gmail.com>
2018-10-25 23:27:25 +0800

    E1

---
+++ Abdon_e1.py
@@ -0,0 +1,14 @@
+data = {"name":"Ericka Delmonte Abdon",
+        "spirit animal":"Bird",
+        "reason":"I like the idea of being free",
+        "hobby":"Read",
+        "profession":"Science Teacher"
+}
+
+print("I am {}".format(data["name"]))
+print("My spirit animal is {}".format(data["spirit animal"]))
+print("because {}".format(data["reason"]))
+print("When not in school I love to {}".format(data["hobby"]))
+print("I dream of being a {}".format(data["profession"]))
+
+
